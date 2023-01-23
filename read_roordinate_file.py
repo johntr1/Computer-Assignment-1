07ee1115413bc37e6a716fa3400473d64774d785
Martind1d <martin.diderholm@gmail.com>
2023-01-23 16:15:18 +0100

    new fun

--- read_roordinate_file.py
+++ read_roordinate_file.py
@@ -2,8 +2,18 @@ import numpy as np
 def read_coordinate_file(filename):
     #Open file in read mode and get data
     with open(f'{filename}',mode='r') as file:
-        coors=file.read()
-        print(coors)
-        print('hej')
+        line=file.readline()
+        l=''
+        while line:
+            print(line)
+            line = line.replace('{', '')
+            line = line.replace('}', '')
+            line = line.replace('\n','')
+            print(line)
+            l=l+line
+            print(l)
+            line=file.readline()
+    l=l.split(',')
+    print(l)
 
 read_coordinate_file('SampleCoordinates.txt')
