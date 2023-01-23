430b622b27973fe357b225035f142d95ecd248ea
Martind1d <martin.diderholm@gmail.com>
2023-01-23 14:57:41 +0100

    a

--- main1.py
+++ main1.py
@@ -13,3 +13,7 @@ csr_matrix((data, ij), shape=(M, N))
 plot_points(coord_list, indices)
 
 find_shortest_path(graph, start_node, end_node)
+
+plot_points(coord_list, indices, path)
+
+construct_fast_graph_connections(coord_list, radius)
