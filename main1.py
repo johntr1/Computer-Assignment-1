181aa24816d80ad645604bc171dee3d907ba9cad
Martind1d <martin.diderholm@gmail.com>
2023-01-23 14:52:48 +0100

    new

--- main1.py
+++ main1.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-=======
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-def fun():
-    print('Hello')
-fun()
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
->>>>>>> a3ed93c84facf2b1ea45faf66f0635520cba4058
-
-
 
 read_coordinate_file(filename)
 
