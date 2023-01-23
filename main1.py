1c36858a7f8df36757ede49990138f70e88c5418
Martind1d <martin.diderholm@gmail.com>
2023-01-23 14:55:52 +0100

    new names

--- main1.py
+++ main1.py
@@ -2,3 +2,14 @@
 read_coordinate_file(filename)
 
 
+plot_points(coord_list)
+
+construct_graph_connections(coord_list, radius)
+
+n construct_graph(indices, distance, N)
+
+csr_matrix((data, ij), shape=(M, N))
+
+plot_points(coord_list, indices)
+
+find_shortest_path(graph, start_node, end_node)
