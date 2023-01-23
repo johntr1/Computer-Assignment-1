39eb07363c0973cc7ce1bf82d22a73aa1f674b79
Martind1d <martin.diderholm@gmail.com>
2023-01-23 15:30:42 +0100

    new fun

---
+++ read_roordinate_file.py
@@ -0,0 +1,8 @@
+import numpy as np
+def read_coordinate_file(filename):
+    #Open file in read mode and get data
+    with open(f'{filename}',mode='r') as file:
+        coors=file.read()
+        print(coors)
+
+read_coordinate_file('SampleCoordinates.txt')
