13d05ba7b43f882f0bb1edfcef78b56677bcc05b
Martind1d <martin.diderholm@gmail.com>
2023-01-25 17:39:47 +0100

    new fun

--- construct_graph.py
+++ construct_graph.py
@@ -1,4 +1,14 @@
 
 
+import numpy as np
+from scipy.sparse import csr_matrix
 
 def construct_graph(indices, distance, N):
+
+    indicesT=indices.T
+    data=distance
+    row=indicesT[0][:]
+    col=indicesT[1][:]
+    csr=csr_matrix((data, (row, col)), shape=(2000, N))
+    print(*csr)
+
--- main1.py
+++ main1.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
 
 from read_coordinate_file import *
 from plot_points import *
 from construct_graph_connections import *
+from construct_graph import *
 
 
 filename = 'HungaryCities.txt'
@@ -11,15 +13,19 @@ radius = 0.005
 print("Read_coordinate_file")
 coord_list = read_coordinate_file(filename)  # M
 
+
 print("construct_graph_connections")
 li_indices, li_distance = construct_graph_connections(coord_list, radius)  # J
 
+N=len(li_indices)
+print(N)
+
 print("plot_points")
-plot_points(coord_list, li_indices)
+#plot_points(coord_list, li_indices)
 
-construct_graph(indices, distance, N)  # M
+construct_graph(li_indices, li_distance, N)  # M
 
-csr_matrix((data, ij), shape=(M, N))
+
 
 find_shortest_path(graph, start_node, end_node)
 
