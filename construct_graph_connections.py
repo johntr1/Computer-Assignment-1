ae72591432e61c3331cff4e5dbb398adddf86b1d
john-tran <johntran1@hotmail.se>
2023-01-27 15:50:37 +0100

    Fixed dubletter

--- construct_graph_connections.py
+++ construct_graph_connections.py
@@ -10,7 +10,9 @@ def construct_graph_connections(coord_list, radius):
     li_distance = []
     for i in range(len(coord_list)):
         for j in range(len(coord_list)):
-
+            # Checks for reverse order duplicates and skips if true
+            if i > j:
+                continue
             # Define the x and y for the distance formula for readability
             x1 = coord_list[i, 0]
             y1 = coord_list[i, 1]
@@ -18,16 +20,8 @@ def construct_graph_connections(coord_list, radius):
             y2 = coord_list[j, 1]
             # Distance formula: distance=√((x2 – x1)² + (y2 – y1)²) (taken from maths book)
             distance = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-            # Define indices as reverse to control duplicate connections
-            indices = [i, j]
-            indices.reverse()
             # Checks for radius and that it is not the same coordinate
             if distance <= radius and distance != 0:
-                # Controls for duplicates in the indices list (NEEDS TO BE FIXED FOR PERFORMANCE)
-                #if indices in li_indices:
-                #    continue
-                # Append the indices and distance if all requirements are met
-                #else:
                 li_indices.append([i, j])
                 li_distance.append(distance)
     # Convert the lists to ndarrays
--- find_shortest_path.py
+++ find_shortest_path.py
@@ -2,10 +2,11 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import shortest_path
 
+# Om du ser detta har jag fixat så att det inte finns dubletter. Det är därför directed=False nu
 
 def find_shortest_path(graph, start_node, end_node):
-    dist_matrix, predecessors = shortest_path(graph, method='D', directed=True, return_predecessors=True, indices=start_node)
+    dist_matrix, predecessors = shortest_path(graph, method='D', directed=False, return_predecessors=True, indices=start_node)
     path = dist_matrix[end_node]
     print(dist_matrix)
     print(predecessors)
-    #print(predecessors)
+
