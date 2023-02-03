e935f5cc1bf6519ffd869a0410c25d237e0c4b77
Martind1d <martin.diderholm@gmail.com>
2023-02-03 13:26:07 +0100

    new fun

--- construct_fast_graph_connections.py
+++ construct_fast_graph_connections.py
@@ -1,17 +1,18 @@
-
+import numpy as np
+import math
 from scipy import spatial
 def construct_fast_graph_connections(coord_list, radius):
     li_indices = []
     li_distance = []
     tree=spatial.cKDTree(coord_list)
     indices=tree.query_ball_point(coord_list, r=radius)
-    for j in indices:
+    for a,j in enumerate(indices):
         for i in j:
-            if j > i:
+            if a > i:
                 continue
-            append.li_indices([j i])
-            distance = math.sqrt((coord_list[0][j] - coord_list[0][i]) ** 2 + (coord_list[1][j] - coord_list[1][i]) ** 2)
-            append.li_distance(distance)
+            li_indices.append([j, i])
+            distance = math.sqrt((coord_list[a][0] - coord_list[i][0]) ** 2 + (coord_list[a][1] - coord_list[i][1]) ** 2)
+            li_distance.append(distance)
     print(indices)
     return li_indices, li_distance
 
