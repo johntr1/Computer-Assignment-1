2be79756eae843b20513ccc5e00372255979c4b3
john-tran <johntran1@hotmail.se>
2023-05-22 01:21:08 +0200

    Hjälpt kevin

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-import numpy as np
-from scipy import spatial
-x, y = np.mgrid[0:4, 0:4]
-points = np.c_[x.ravel(), y.ravel()]
-tree = spatial.cKDTree(points)
-tree.query_ball_point([2, 0], 1)
-print(points)
+import math
+import statistics
 
+
+
+x = 5
+for i in range(x):
+    print("John e gay")
+
+
+while i == 4:
+    print("Kevin e gay")
+    i -= 1
+
+print(f'Kevin är {i:.5f} år gammal')
+print('Hello world')
